774b4dcacadc78bd2d35c491826fec041dfc956b
Killian' DAL-CIN <killian.dalcin@gmail.com>
2023-08-16 11:59:11 +0200

    done

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,58 @@
+class FlatsController < ApplicationController
+  def index
+    if params[:q].present?
+      search_query = params[:q].downcase
+      @flats = Flat.where("LOWER(name) LIKE ?", "%#{search_query}%")
+    else
+      @flats = Flat.all
+    end
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to @flat, notice: "Flat was successfully created."
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @flat = Flat.find(params[:id])
+  end
+
+  def update
+    @flat = Flat.find(params[:id])
+    if @flat.update(flat_params)
+      redirect_to @flat, notice: "Flat was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @flat = Flat.find(params[:id])
+    @flat.destroy
+    redirect_to flats_url, notice: "Flat was successfully destroyed."
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(
+      :name,
+      :address,
+      :description,
+      :price_per_night,
+      :number_of_guests
+    )
+  end
+end
